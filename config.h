ab5195c9f068c1d3ca9de367d891083e8709a2ea
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2023-05-31 21:01:05 +0800

    feat: use select rather than epoll

--- config.h
+++ config.h
@@ -76,7 +76,7 @@
 
 /* Test for polling API */
 #ifdef __linux__
-#define HAVE_EPOLL 1
+#define HAVE_SELECT 1
 #endif
 
 #if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
